3bdfeafe6961ac6952865c0cd6a361e6bb51c36b
Sadat <sadatsaon2020@gmail.com>
2025-10-02 23:05:59 +0600

    second task done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 // task 01
-const studentInfo= {
-    id:'NA-323A66',
-    name:'Sadat Saon',
-    birthMonth:12,
-    favoriteColor:'skyblue'
+const studentInfo = {
+    id: 'NA-323A66',
+    name: 'Sadat Saon',
+    birthMonth: 12,
+    favoriteColor: 'skyblue'
 };
-console.log('StudentInfo',studentInfo);
+console.log('StudentInfo', studentInfo);
+
+// task 02
+// object and data management
+// using object lateral method
+const taskObject = {
+    id: 'NA-323A66' + '_' + Date.now(),
+    title: 'full-stack web development',
+    description: 'completed html,css and now learning javaScript',
+    priority: 3,
+    completed: false,
+    createdDate: new Date(),
+    dueDate: '2025-10-03',
+    category: 'Assignment'
+}
+console.log('taskObject', taskObject);
